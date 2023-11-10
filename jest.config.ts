b1a6f51eb667f3eb865862af76f18778eeeb2260
I Made Budi Dharma Wipraja <wiprajad@gmail.com>
2023-11-10 07:22:29 +0700

    chore: test codecov gh action

--- jest.config.ts
+++ jest.config.ts
@@ -8,4 +8,12 @@ export default {
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
