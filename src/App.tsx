2541e30b74c8d59e680bc0ef72aea9855030b4b0
I Made Budi Dharma Wipraja <wiprajad@gmail.com>
2023-11-11 18:07:45 +0700

    chore: fix unit test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { TEXT } from "./config/env";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,7 +21,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-        <h1>{import.meta.env.VITE_APP_TEXT}</h1>
+        <h1>{TEXT}</h1>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
